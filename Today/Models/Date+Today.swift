ff6c40dfd98c2df3e7d9254b21621bbb004fad18
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-29 14:08:52 +0300

    Displaying cell info Section 1: Format the date and time

---
+++ Today/Models/Date+Today.swift
@@ -0,0 +1,29 @@
+//
+//  Date+Today.swift
+//  Today
+//
+//  Created by Sergei Poluboiarinov on 2022-11-29.
+//
+
+import Foundation
+
+extension Date {
+    var dateAndTimeText: String {
+        let timeText = formatted(date: .omitted, time: .shortened)
+        if Locale.current.calendar.isDateInToday(self) {
+            let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
+            return String(format: timeFormat, timeText)
+        } else {
+            let dataText = formatted(.dateTime.month(.abbreviated).day())
+            let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
+            return String(format: dateAndTimeFormat, dataText, timeText)
+        }
+    }
+    var dayText: String {
+        if Locale.current.calendar.isDateInToday(self) {
+            return NSLocalizedString("Today", comment: "Today due date description")
+        } else {
+            return formatted(.dateTime.month().day().weekday(.wide))
+        }
+    }
+}
