3a271658cf4da05cdfba06c665b4b6716b76ff94
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-30 20:45:32 +0300

    Section 2: Organize view controllers

--- Today/Models/Date+Today.swift
+++ Today/Models/Date+Today.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 extension Date {
-    var dateAndTimeText: String {
+    var dayAndTimeText: String {
         let timeText = formatted(date: .omitted, time: .shortened)
         if Locale.current.calendar.isDateInToday(self) {
             let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
             return String(format: timeFormat, timeText)
         } else {
-            let dataText = formatted(.dateTime.month(.abbreviated).day())
+            let dateText = formatted(.dateTime.month(.abbreviated).day())
             let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
-            return String(format: dateAndTimeFormat, dataText, timeText)
+            return String(format: dateAndTimeFormat, dateText, timeText)
         }
     }
     var dayText: String {
